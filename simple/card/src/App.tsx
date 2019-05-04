dbfbda55697d36c8dd64362b6d80325e5a2747e9
Denis Khaidarshin <denis.khaidarshin@arcadia.spb.ru>
2019-05-04 21:43:05 +0300

    card

---
+++ simple/card/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card } from './components/card';
+function App() {
+  return (
+    <div className="App">
+      <Card></Card>
+    </div>
+  );
+}
+
+export default App;
---
+++ simple/card/src/components/card/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+
+import './card.scss';
+
+
+export class Card extends Component {
+    public render(){
+        return (
+            <div>Hello</div>
+        )
+    }
+}
