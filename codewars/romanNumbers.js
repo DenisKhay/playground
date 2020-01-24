d3719586652d2055d67069d554f079db6c058691
Denis Khaidarshin <denis.khaidarshin@arcadia.spb.ru>
2020-01-24 20:00:36 +0300

    wip

--- codewars/romanNumbers.js
+++ codewars/romanNumbers.js
@@ -6,25 +6,5 @@ function findRoman(val) {
     let second = 5;
     let third = 1;
 
-    let am = Math.floor((val % first) / third) * third;
 
-    if(am + third === second) {
-        num += map.get(third) + map.get(second)
-    } else if(am > second) {
-
-    } else {
-        num = new Array(am / third).fill(map.get(third)).join('');
-    }
-
-    first = 100;
-    second = 50;
-    third = 10;
-
-    am = Math.floor((val % first) / third) * third;
-
-    if(am + third === second) {
-        num += map.get(third) + map.get(second)
-    } else {
-        num = new Array(am / third).fill(map.get(third)).join('');
-    }
 }
