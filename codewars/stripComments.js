7411a7dca459860344df2e6170d6ae0537ac19c2
Denis Khaidarshin <denis.khaidarshin@arcadia.spb.ru>
2020-04-21 20:30:23 +0300

    done cata stripComments

---
+++ codewars/stripComments.js
@@ -0,0 +1,11 @@
+function solution(input, markers) {
+    return markers.reduce((str, marker) => {
+        const regexp = new RegExp(`\\s*\\${marker}.*?(\\n|$)`, 'g');
+        const st = str.replace(regexp, '$1');
+        return st;
+    }, input)
+};
+
+const here = solution("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"])
+
+console.log('done', here);
