cfc3d8c313abbb3abbdea9747d2a06f228c30313
denis.khaidarshin <denis.khaidarshin@arcadia.spb.ru>
2020-08-23 18:33:27 +0300

    removed old projects

--- pipeline-runner/modules/get-branches.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-module.exports = (params) => {
-    const {workspace, password, repository, username} = params;
-    const url = `https://api.bitbucket.org/2.0/repositories/${workspace}/${repository}/refs/branches`;
-    return axios.get(url, {auth: {username, password}});
-};
--- pipeline-runner/modules/get-pipelines.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-module.exports = (params) => {
-    const {workspace, password, repository, username} = params;
-    const url = `https://api.bitbucket.org/2.0/repositories/${workspace}/${repository}/pipelines/?pagelen=10&sort=-created_on`;
-    return axios.get(url, {auth: {username, password}})
-};
