7b37b34d168260f787034c1ba3edf6febfa6d2cd
denis.khaidarshin <dragolietel@gmail.com>
2022-08-04 19:45:12 +0300

    timelne | added initial starter

--- react-rc-table/src/App.tsx
+++ react-rc-table/src/App.tsx
@@ -11,6 +11,7 @@ interface DataType {
     address: string;
     country: string;
     code: string;
+    key: any;
 }
 const data: DataType[] = [...Array(1000)].map(() => {
     return {
@@ -18,7 +19,7 @@ const data: DataType[] = [...Array(1000)].map(() => {
         name: randFirstName(),
         lastName: randLastName(),
         company: randCompanyName(),
-        address: randAddress().toString(),
+        address: randAddress().city,
         country: randCountry(),
         code: randAbbreviation()
     }
@@ -124,32 +125,33 @@ const findFirstAndLast = (list: NodeList, scrollTop: number, containerHeight: nu
     const nearestIndexBottom = findNearestIndexTo(bottomTarget, list, (el: HTMLElement) => el.offsetTop);
     return [nearestIndexTop, nearestIndexBottom];
 }
-
+const getScrollContainer = (): any => document.getElementsByClassName('rc-table-body')[0];
+const getNodesContainer = () => document.getElementsByClassName('rc-table-tbody')[0];
 function App() {
   const tableRef = useRef(null);
   const [span, setSpan] = useState([0, 99])
   const internalData = useMemo(() => data.slice(span[0], span[1]), [span])
 
   useEffect(() => {
-      const outerTableBody = document.getElementsByClassName('rc-table-body')[0];
-      const innerTableBody = document.getElementsByClassName('rc-table-tbody')[0];
+      const scrollContainer = getScrollContainer();
+      const nodesContainer = getNodesContainer();
       let id: number;
-      let scrollTop = outerTableBody.scrollTop;
-
+      let scrollTop = scrollContainer.scrollTop;
+      let timeoutId: any;
       const listenerCallback = () => {
-          cancelAnimationFrame(id);
-          id = requestAnimationFrame(() => {
-              const [topNearest, bottomNearest] = findFirstAndLast(innerTableBody.childNodes, outerTableBody.scrollTop, outerTableBody.clientHeight)
-              const direction = outerTableBody.scrollTop < scrollTop ? 'up' : 'down';
-              scrollTop = outerTableBody.scrollTop;
-              console.log('run raf')
+          clearTimeout(timeoutId)
+          timeoutId = setTimeout(() => {
+              const [topNearest, bottomNearest] = findFirstAndLast(nodesContainer.childNodes, scrollContainer.scrollTop, scrollContainer.clientHeight)
+              const direction = scrollContainer.scrollTop < scrollTop ? 'up' : 'down';
+              scrollTop = scrollContainer.scrollTop;
+              console.log('run tmt')
               if(direction === 'down') {
                   setSpan((span) => {
-                      if((bottomNearest! + 5) > span[1]) {
+                      if((bottomNearest! + 50) > span[1]) {
                           console.log('set span')
                           return [
                               span[0],
-                              span[1] + 10
+                              span[1] + 50
                           ]
                       }
                       return span;
@@ -157,11 +159,39 @@ function App() {
               } else {
 
               }
+          }, 100);
+          cancelAnimationFrame(id);
+          id = requestAnimationFrame(() => {
+
           })
       }
-      outerTableBody.addEventListener('scroll', listenerCallback);
-      return () => outerTableBody.removeEventListener('scroll', listenerCallback);
+      scrollContainer.addEventListener('scroll', listenerCallback);
+      return () => scrollContainer.removeEventListener('scroll', listenerCallback);
   }, []);
+  useLayoutEffect(() => {
+      const nodesContainer = getNodesContainer();
+      const lastNode = nodesContainer.childNodes[nodesContainer.childNodes.length - 1];
+      console.dir(lastNode);
+      (lastNode as any).style.height = '10000px';
+      (lastNode as HTMLElement).classList.add('last')
+  }, []);
+
+  useLayoutEffect(() => {
+      const nodesContainer = getNodesContainer() as HTMLElement;
+      let last = nodesContainer.getElementsByClassName('last')[0] as HTMLElement;
+      if(last) {
+          const height = last.offsetHeight;
+          const offset = last.offsetTop;
+          const theOffset = nodesContainer.offsetHeight - offset - height + 2;
+          console.log('the offset', theOffset, offset, height);
+          (last as any).style.height = '';
+          last.classList.remove('last');
+          last = nodesContainer.childNodes[nodesContainer.childNodes.length - 1] as HTMLElement;
+          last.classList.add('last');
+          last.style.height = `${height - theOffset}px`;
+      }
+  }, [internalData])
+
   console.log('rerender!')
   return (
     <div className="App">
