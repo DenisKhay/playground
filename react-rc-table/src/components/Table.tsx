0d5dcfa2852eb1daf0408953617495790b201cea
denis.khaydarshin <denis.khaydarshin@gmail.com>
2025-10-17 15:24:54 +0300

    removed outdated

--- react-rc-table/src/components/Table.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react';
-
-export const Table: FC<any> = () => {
-    return (
-        <>null</>
-    )
-}
--- timeline/src/components/ScrollbarControl/ScrollbarControl.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-import { scrollBar } from './ScrollbarControl.module.scss'
-export const ScrollbarControl: FC<unknown> = () => {
-    return (
-        <div className={scrollBar}>
-
-        </div>
-    )
-}
